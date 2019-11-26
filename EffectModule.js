04c803aed509e58c1e5e8a9a10afeb10b784485e
Glitch (hello-webpage) <none>
2019-11-26 04:36:46 +0000

    👣💎 Checkpoint ./EffectModule.js:7638468/582

--- EffectModule.js
+++ EffectModule.js
@@ -10,6 +10,7 @@ class EffectModule {
     
     this._panner = new PannerNode(this._context);
     this.input.connect(this._panner).connect(this._wet).connect(this.output);
+    this.input.connect(this._panner).connect(this._dry).connect(this.output);
     this._panner.panningModel = "HRTF";
     this._panner.positionY.value = 0.1;
     this.x;
@@ -31,8 +32,8 @@ class EffectModule {
   }
 
   async initialize() {
-    const file = 'https://cdn.glitch.com/c44791c8-02d2-4f00-87fb-fa398dfbec75%2Fwater-swirl.wav?v=1574729613476';
-    //const file = 'https://cdn.glitch.com/c44791c8-02d2-4f00-87fb-fa398dfbec75%2Fbreath.wav?v=1574729612779';
+    //const file = 'https://cdn.glitch.com/c44791c8-02d2-4f00-87fb-fa398dfbec75%2Fwater-swirl.wav?v=1574729613476';
+    const file = 'https://cdn.glitch.com/c44791c8-02d2-4f00-87fb-fa398dfbec75%2Fbreath.wav?v=1574729612779';
     const response = await fetch(file);
     const arrayBuffer = await response.arrayBuffer();
     this._convolver.buffer = await this._context.decodeAudioData(arrayBuffer);
@@ -44,8 +45,8 @@ class EffectModule {
   param1(value, when) {
     this.x = value;
     const later = this._context.currentTime + 0.07;
-    this._panner.positionX.linearRampToValueAtTime(Math.sin(this.x), later);
-    this._panner.positionZ.linearRampToValueAtTime(Math.cos(this.x), later);
+    //this._panner.positionX.linearRampToValueAtTime(Math.sin(this.x), later);
+    //this._panner.positionZ.linearRampToValueAtTime(Math.cos(this.x), later);
   }
 
   param2(value, when) {
@@ -57,18 +58,22 @@ class EffectModule {
 
   param3(event, userX, userY) {
     const rect = event.target.getBoundingClientRect();
+    const later = this._context.currentTime + 0.07;
     userX = event.clientX - rect.left;
     userY = event.clientY - rect.top;
 
-    const x = (userX - 100)
-    const z = (userY - 100)
-    this._panner.positionX.linearRampToValueAtTime
+    const x = (30 * 3.14 * (-1))/ (userX - 100);
+    const z = (30 * 3.14) / (userY - 100);
+    const y = (userX - 100) / 100;
+    this._panner.positionX.linearRampToValueAtTime(Math.sin(x), later);
+    this._panner.positionZ.linearRampToValueAtTime(Math.cos(z), later);
+    this._panner.positionY.linearRampToValueAtTime(y, later);
     
-    const frequency = (userX / 200) * 880 + 110;
-    const cutoff = (1 - userY / 200) * 3520 + 440;
-    const later = this._context.currentTime + 0.04;
+    //const frequency = (userX / 200) * 880 + 110;
+    //const cutoff = (1 - userY / 200) * 3520 + 440;
+    //const later = this._context.currentTime + 0.04;
     //this.input.gain.exponentialRampToValueAtTime(frequency, later);
-    this._biquad.frequency.exponentialRampToValueAtTime(frequency, later);
+    //this._biquad.frequency.exponentialRampToValueAtTime(frequency, later);
   }
 }
 

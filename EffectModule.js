156957acac63f630b9a4f6851c5615b39377a5f4
Glitch (hello-webpage) <none>
2019-11-26 00:51:40 +0000

    🏬🐑 Checkpoint ./EffectModule.js:7638468/2448

--- EffectModule.js
+++ EffectModule.js
@@ -14,18 +14,6 @@ class EffectModule {
     this._panner.positionY.value = 0.1;
     this.x;
     
-    this._waveshaper = new WaveShaperNode(this._context);
-    this._send = new GainNode(this._context);
-    this._delay = new DelayNode(this._context);
-    this._feedback = new GainNode(this._context);
-    this._plate = new ConvolverNode(this._context);
-    this._cabinet = new ConvolverNode(this._context);
-    
-    this._waveshaper.connect(this._cabinet).connect(this._wet);
-    this._waveshaper.connect(this._send);
-    this._send.connect(this._delay).connect(this._feedback).connect(this._delay);
-    this._delay.connect(this._plate)
-    
     // this._compressor = new DynamicsCompressorNode(this._context);
     // this._makeup = new GainNode(this._context);
     // this._compressor.connect(this._makeup).connect(this.output);
@@ -33,18 +21,20 @@ class EffectModule {
     this._wet.gain.value = 0.5;
     this._dry.gain.value = 0.5;
 
-    this.input.connect(this._wet).connect(this.output);
-    //this.input.connect(this._convolver).connect(this._wet).connect(this.output);
+    //this.input.connect(this._wet).connect(this.output);
+    this.input.connect(this._convolver).connect(this._wet).connect(this.output);
     this.input.connect(this._dry).connect(this.output);
   }
 
   async initialize() {
-    const file = 'https://cdn.glitch.com/c44791c8-02d2-4f00-87fb-fa398dfbec75%2Fpapercrumble.wav.crdownload.wav?v=1574717761204';
+    const file = 'https://cdn.glitch.com/c44791c8-02d2-4f00-87fb-fa398dfbec75%2Fguitar.aiff?v=1574717660523';
+    //const file = 'https://cdn.glitch.com/c44791c8-02d2-4f00-87fb-fa398dfbec75%2Fpapercrumble.wav.crdownload.wav?v=1574717761204';
     const response = await fetch(file);
     const arrayBuffer = await response.arrayBuffer();
     this._convolver.buffer = await this._context.decodeAudioData(arrayBuffer);
+    
   }
-
+  
   close() {}
 
   param1(value, when) {
@@ -56,7 +46,6 @@ class EffectModule {
 
 
   param2(value, when) {
-    
     
     // const scaledValue = value / 400.0;
     // this._compressor.threshold.value = 20;

005806dc975d261fc31bd100bcabbf4d7bc09598
Glitch (hello-webpage) <none>
2019-11-26 06:34:50 +0000

    💂👞 Checkpoint ./GeneratorModule.js:7638468/2297

--- GeneratorModule.js
+++ GeneratorModule.js
@@ -11,8 +11,9 @@ class GeneratorModule {
     this._amp = new GainNode(this._context);
 
     this._osc.type = 'sine';
-    this._osc.frequency.value = 440;
-    //this._osc.frequency.value = Lib220a.mtof(60);
+    this._notePitch = null;
+    this._osc.frequency.value = this._notePitch;
+
     this._amp.gain.value = 0.0;
     this._lfo.frequency.value = 8;
     this._depth.gain.value = 200;
@@ -22,58 +23,29 @@ class GeneratorModule {
     this._osc.start();
     this._lfo.start();
     
-    this._notePitch;
     this._volume = 1.0;
     this._currentPitches = [];
   }
 
   async initialize() {}
 
-  close() {
-    this._osc.stop();
-    this._lfo.stop();
-  }
-
   noteOn(pitch, velocity, when) {
-    // const now = this._context.currentTime;
-    // console.log(pitch);
-    // this._osc.frequency.setTargetAtTime(this._notePitch, now, 0.001); 
-    // this._amp.gain.setTargetAtTime(this._volume, now, 0.0001);
-
-//     //Q: What does this code do?
-    const noteOnPitch = Math.floor(Lib220a.mtof(pitch));
-    if (!this._currentPitches.includes(noteOnPitch))
-      this._currentPitches.push(noteOnPitch);
-    
     const now = this._context.currentTime;
-    this._osc.frequency.setTargetAtTime(noteOnPitch, now, 0.001);
-
-//    // Q: What does `1` mean?
-    if (this._currentPitches.length === 1)
-      this._amp.gain.setTargetAtTime(1.0, now, 0.001);
+    console.log(pitch);
+    this._osc.frequency.setTargetAtTime(this._notePitch, now, 0.001); 
+    this._amp.gain.setTargetAtTime(this._volume, now, 0.0001);
   }
 
   noteOff(pitch, velocity, when) {
-    // const now = this._context.currentTime;
-    // this._amp.gain.setTargetAtTime(0.0, now, 0.0001);
-    // close();
-    
-    // Q: What does this code do?
-    const noteOffPitch = Math.floor(pitch);
-    const index = this._currentPitches.indexOf(noteOffPitch);
-    if (index > -1)
-      this._currentPitches.splice(index, 1);
-    
-    // Q: What does `0` mean?
-    if (this._currentPitches.length === 0) 
-      this._amp.gain.setTargetAtTime(0.0, this._context.currentTime, 0.001);
+    const now = this._context.currentTime;
+    this._amp.gain.setTargetAtTime(0.0, now, 0.0001);
   }
 
   param1(value, when) {
-    // const now = this._context.currentTime;
-    // this._notePitch = Math.floor(Lib220a.mtof(value) * 10, now, 0.001);
-    // this._osc.frequency.setTargetAtTime(this._notePitch, this._context.currentTime, 0.001);
-    // return this._notePitch;
+    const now = this._context.currentTime;
+    this._notePitch = Math.floor(Lib220a.mtof(value) * 10, now, 0.001);
+    this._osc.frequency.setTargetAtTime(this._notePitch, this._context.currentTime, 0.001);
+    return this._notePitch;
   }
   
   param2(value, when) {
